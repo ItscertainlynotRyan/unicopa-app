dc2f0842133d27a7a57f0db7e95ecd7b4dc1016e
ItscertainlynotRyan <ryan201195@edu.unipar.br>
2026-05-05 19:39:30 -0300

    subindo projeto

---
+++ App.js
@@ -0,0 +1,143 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  FlatList,
+} from "react-native";
+import GameCard from "./app/components/GameCard";
+import copaData from "./app/assets/data/copaData.json";
+import { useState } from "react";
+import { SectionList } from "react-native";
+
+export default function App() {
+  const [jogos, setJogos] = useState(copaData.jogos);
+  const [dadosCopa, setDadosCopa] = useState(copaData);
+  const agruparPorData = (jogos) => {
+    return jogos.reduce((acc, jogo) => {
+      const data = jogo.data_brasilia;
+      if (!acc[data]) {
+        acc[data] = [];
+      }
+      acc[data].push(jogo);
+      return acc;
+    }, {});
+  };
+  const jogosAgrupados = agruparPorData(jogos);
+  const jogosTratados = Object.keys(jogosAgrupados).map((data) => {
+    return {
+      title: data,
+      data: jogosAgrupados[data],
+    };
+  });
+
+  return (
+    <ImageBackground
+      style={styles.container}
+      source={require("./app/assets/bg-overlay.png")}
+    >
+      <Image style={styles.logo} source={require("./app/assets/unicopa.png")} />
+
+      <Text style={styles.title}>CALENDÁRIO</Text>
+      <SectionList
+        sections={jogosTratados}
+        keyExtractor={(item, index) => item.id || index.toString()}
+        renderItem={() => null}
+        renderSectionHeader={({ section }) => (
+          <View style={styles.card}>
+            <Text style={styles.data}>
+              {section.title.split("-").slice(1).reverse().join("/")}
+            </Text>
+            {section.data.map((jogo) => (
+              <GameCard key={jogo.id} game={jogo} />
+            ))}
+          </View>
+        )}
+      />
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#040b13",
+    alignItems: "center",
+  },
+  logo: {
+    marginTop: 20,
+    width: 200,
+    height: 50,
+    resizeMode: "contain",
+  },
+  title: {
+    marginTop: 10,
+    fontSize: 28,
+    fontWeight: "700",
+    color: "white",
+  },
+  card: {
+    marginTop: 20,
+    backgroundColor: "#0c1b2a",
+    width: 320,
+    borderRadius: 12,
+    padding: 15,
+  },
+  data: {
+    color: "#f2cc2f",
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  jogo: {
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#1e2d3d",
+    paddingBottom: 15,
+  },
+  grupo: {
+    color: "#8fa3b8",
+    fontSize: 12,
+    marginBottom: 10,
+  },
+  linhaPrincipal: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  time: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  bandeira: {
+    width: 28,
+    height: 28,
+    borderRadius: 14,
+  },
+  sigla: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  horario: {
+    alignItems: "center",
+  },
+  hora: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  local: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  subTitulo: {
+    color: "#8fa3b8",
+    fontSize: 12,
+  },
+});
---
+++ app/components/GameCard.jsx
@@ -0,0 +1,65 @@
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+import TimeCard from "./TimeCard";
+
+export default function GameCard({ game }) {
+  return (
+    <View style={styles.jogo}>
+      <Text style={styles.grupo}>
+        GRUPO {game.grupo} {game.confronto}
+      </Text>
+
+      <View style={styles.linhaPrincipal}>
+        <TimeCard siglaTime={game.sigla_casa} />
+        <View style={styles.horario}>
+          <Text style={styles.hora}>{game.hora_brasilia}</Text>
+          <Text style={styles.subTitulo}>VS</Text>
+        </View>
+
+        <TimeCard siglaTime={game.sigla_fora} />
+      </View>
+
+      <View style={styles.local}>
+        <Text style={styles.subTitulo}>{game.estadio}</Text>
+        <Text style={styles.subTitulo}>
+          {game.cidade} • {game.pais}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  jogo: {
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#1e2d3d",
+    paddingBottom: 15,
+  },
+  grupo: {
+    color: "#8fa3b8",
+    fontSize: 12,
+    marginBottom: 10,
+  },
+  linhaPrincipal: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  horario: {
+    alignItems: "center",
+  },
+  hora: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  local: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  subTitulo: {
+    color: "#8fa3b8",
+    fontSize: 12,
+  },
+});
